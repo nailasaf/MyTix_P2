b395078c4e3c5a7aaf0a5c16e593e651b872c34f
naila <nailasafynatul@gmail.com>
2026-04-19 22:41:26 +0700

    initial commit

---
+++ app/src/main/java/com/app/mytix/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.app.mytix
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // nampilin layout splash
+        setContentView(R.layout.activity_splash)
+
+        // delay 2 detik lalu pindah ke MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 5000)
+    }
+}
